cd17260b9698640163134c6f0ad17a4fe8137505
RubenSuiteSeven <rubenjansen@suiteseven.nl>
2020-10-08 15:25:26 +0200

    Main file commit

--- main.py
+++ main.py
@@ -2,5 +2,21 @@
 import pandas as pd;
 
 #Dataframe
-course_availability = pd.read_csv("data/course_availability.csv");
+df1 = pd.read_csv("data/course_availability.csv");
+
+#Explore data
+
+def explore_data(dataset):
+    columns = dataset.columns;
+    columns_total = len(dataset.columns);
+    cells = dataset.size;
+    rows = dataset.size/len(dataset.columns);
+    datatypes = dataset.dtypes;
+    missingvalues = dataset.isna().sum();
+    return  (print("Columns ", columns), print("Columns total ", columns_total), print("Cells ", cells), print("Rows ", rows), print("Datatypes ", datatypes), print("Missigvalues ", missingvalues))
+
+explore_data(df1);
+
+
+
 
