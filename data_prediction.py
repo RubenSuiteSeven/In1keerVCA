5823ce77950f8ebdd761888210a1cbbb6c94869a
RubenJansen <rubenjansen@suiteseven.nl>
2020-11-03 10:15:23 +0100

    Commit datascience VCA

--- data_prediction.py
+++ data_prediction.py
@@ -1,7 +1,6 @@
 #Imports
 import pandas as pd;
 import mysql.connector;
-import pandasql as ps;
 import matplotlib.pyplot as plt;
 import seaborn as sns;
 
@@ -13,8 +12,8 @@ mydb = mysql.connector.connect(user='root', password='admin',
 
 
 #Variables
-customer_lat = 52.3953334;
-customer_lng = 4.9493602;
+customer_lat = 52.48918;
+customer_lng = 4.654158;
 customer_course = 1;
 date = '10-01-20';
 
@@ -31,16 +30,20 @@ locations = locations[locations.deleted_at.isnull()];
 course_availability = course_availability[course_availability.pontifex_id.isnull()];
 
 #Data prediction
-# course_availability_2 = pd.read_sql_query("""SELECT * FROM course_availability JOIN availability ON course_availability.availability_id=availability.id JOIN locations l on l.id = availability.locations_id WHERE courses_id = 1 AND spots_taken is not null order by date""", mydb);
-# course_availability_2 = course_availability_2[course_availability_2.pontifex_ids.isnull()];
+course_availability_2 = pd.read_sql_query("""SELECT * FROM course_availability JOIN availability ON course_availability.availability_id=availability.id JOIN locations l on l.id = availability.locations_id WHERE spots_taken is not null order by date""", mydb);
+course_availability_2 = course_availability_2[course_availability_2.pontifex_ids.isnull()];
 
 #Remove pontifex locations
-location_df = pd.read_sql_query("""SELECT * FROM locations JOIN location_meta ON locations.id=location_meta.locations_id WHERE pontifex_ids IS NULL AND pontifex_active = 0 AND status = "PUBLISHED" """, mydb);
-
-#Distance locations
-query = """SELECT ROUND(6371 * acos(cos(radians(%s)) * cos(radians(lat)) * cos(radians(lng) - radians(%s)) + sin(radians(%s)) * sin(radians(lat)))) as distance,lat,lng,city,locations_id from location_meta HAVING distance<=20  order by distance""";
+location_df = pd.read_sql_query("""SELECT * FROM locations JOIN location_meta ON locations.id=location_meta.locations_id WHERE pontifex_ids IS NOT NULL AND pontifex_active = 1 AND status = "PUBLISHED" """, mydb);
+query = """SELECT ROUND(6371 * acos(cos(radians(%s)) * cos(radians(lat)) * cos(radians(lng) - radians(%s)) + sin(radians(%s)) * sin(radians(lat)))) as distance,lat,lng,city,locations_id from location_meta HAVING distance<=30  order by distance""";
 closest_locations = pd.read_sql_query(query, mydb, params=[customer_lat, customer_lng, customer_lat]);
-print(closest_locations);
 
 for x in location_df.id:
-    closest_locations.drop[closest_locations[closest_locations.locations_id] == x]
+    closest_locations = closest_locations[closest_locations.locations_id != x];
+
+print(closest_locations);
+
+for x in closest_locations.locations_id:
+    course_availability_2 = course_availability_2[course_availability_2.locations_id != x];
+    print(course_availability_2);
+
